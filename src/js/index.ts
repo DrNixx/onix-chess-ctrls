e696f86485d5762d1ad7ac1fccb2c6b5f2be5ec6
DrNixx <drnixx@gmail.com>
2020-03-09 21:44:44 +0300

    Fix export

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,4 +3,5 @@ export { SizeSelector, SizeSelectorProps } from './ctrls/SizeSelector';
 export { SquareSelector, SquareSelectorProps } from './ctrls/SquareSelector';
 export { StartPosSelector, StartPosSelectorProps } from './ctrls/StartPosSelector';
 export { WhoMoveSelector, WhoMoveSelectorProps } from './ctrls/WhoMoveSelector';
+export { TextWithCopy, TextWithCopyProps } from './ctrls/TextWithCopy';
 export { Intl } from './Intl';
