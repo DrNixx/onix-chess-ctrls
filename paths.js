3ee101c0f72fe07af9a905c1ce05872f36ce28e3
DrNixx <drnixx@gmail.com>
2020-12-03 06:02:10 +0300

    Add styles

--- paths.js
+++ paths.js
@@ -7,16 +7,19 @@ module.exports = {
 	build: {
 		html: base + '/',
 		assets: assets + '/',
-		scripts: js + '/',
+		scripts: assets + '/js/',
+		styles: assets + '/css/',
 	},
 	src: {
 		scripts: './src/js/index.ts',
+		styles: ['./src/css/*.scss', '!./src/css/_*.scss'], 
 		tests: './src/test/index.ts',
 		templates: './src/templates/',
 		nunj: 'src/templates/*.nunj',
 	},
 	watch: {
 		nunj: 'src/templates/**/*.nunj',
+		styles: 'src/css/**/*.scss', 
 		scripts: 'src/js/**/*.ts',
 	},
 	clean: base + '/*',
